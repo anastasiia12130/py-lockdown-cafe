ef6b1016bb3c7a6a4c6a443a1fa8ff1832bdf124
Anastasiia Skorodied <anastasia.skoroded@gmail.com>
2025-07-07 20:01:22 +0300

    Changed double quotes to single quotes

--- app/cafe.py
+++ app/cafe.py
@@ -14,15 +14,15 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"Entry denied! {visitor["name"]} is not vaccinated."
+                f"Entry denied! {visitor['name']} is not vaccinated."
             )
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError(
-                f"Entry denied! {visitor["name"]}'s vaccine has expired."
+                f"Entry denied! {visitor['name']}'s vaccine has expired."
             )
         elif not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError(
-                f"Entry denied! {visitor["name"]} is not wearing a mask."
+                f"Entry denied! {visitor['name']} is not wearing a mask."
             )
 
         return f"Welcome to {self.name}"
